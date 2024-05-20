6f1f5a33096b6cdd4b50f4faba1f4912ad55d1d3
EL11HALCON <miguel_cancer_0@hotmail.com>
2024-05-19 19:12:35 -0500

    implementacion del css

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import "../App.css";  // Asegúrate de que los estilos CSS se mantengan importados
 
 const Formulario = () => {
     const [formData, setFormData] = useState({
@@ -42,12 +41,11 @@ const Formulario = () => {
                 throw new Error('Error al enviar el formulario');
             }
     
-            // Extract user data from response
             const responseData = await response.json();
             const userId = responseData.id;
             const role = responseData.role;
     
-            // Clear form data
+          
             setFormData({
                 name: '',
                 email: '',
@@ -56,91 +54,81 @@ const Formulario = () => {
                 address: ''
             });
     
-            // Compose success alert message
-            alert(`Cliente registrado con éxito. El número de cliente es ${userId} y su rol es ${role}`);
+            
+            alert(`Cliente registrado con éxito. El número de cliente es ${userId}, y su rol es ${role}`);
         } catch (error) {
             alert('Ocurrió un error al enviar el formulario. Por favor, intenta nuevamente.');
             console.error(error);
         }
     };
+
     return (
-        <div className="form_container_contact">
-            <div className="form_header">
-                <h1 className="form_title">Registrar usuario</h1>
-            </div>
-            <div className="form_body">
-                <form onSubmit={handleSubmit}>
-                    <div className="form">
-                        <label className="form_label">Name:</label>
-                        <div className="form_input-ico">
-                            <input
-                                type="text"
-                                className="form_input"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="form">
-                        <label className="form_label">Email:</label>
-                        <div className="form_input-ico">
-                            <input
-                                type="email"
-                                className="form_input"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="form">
-                        <label className="form_label">Password:</label>
-                        <div className="form_input-ico">
-                            <input
-                                type="password"
-                                className="form_input"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="form">
-                        <label className="form_label">Confirm Password:</label>
-                        <div className="form_input-ico">
-                            <input
-                                type="password"
-                                className="form_input"
-                                name="confirmPassword"
-                                value={formData.confirmPassword}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="form">
-                        <label className="form_label">Address:</label>
-                        <div className="form_input-ico">
-                            <input
-                                type="text"
-                                className="form_input"
-                                name="address"
-                                value={formData.address}
-                                onChange={handleInputChange}
-                            />
-                        </div>
-                    </div>
-                    <div className="form">
-                        <button type="submit">Enviar</button>
-                    </div>
-                </form>
-            </div>
+        <div className="formulario-fondo d-flex justify-content-center align-items-center min-vh-100 bg-dark">
+            <form className="advanced-form p-4 rounded bg-secondary text-white w-50" onSubmit={handleSubmit}>
+            <h1 className="text-center text-white mb-4">Registrar Usuario</h1>
+                <div className="form-group mb-3">
+                    <label>Nombre</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        placeholder="Ingrese su nombre"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <label>Correo</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        placeholder="Ingrese su correo"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <label>Contraseña</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        name="password"
+                        placeholder="Ingrese su contraseña"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <label>Confirma tu Contraseña</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        name="confirmPassword"
+                        placeholder="Confirme su contraseña"
+                        value={formData.confirmPassword}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <label>Dirección</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="address"
+                        placeholder="Ingrese su dirección"
+                        value={formData.address}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <button type="submit" className="btn btn-success">Enviar</button>
+            </form>
         </div>
     );
-}
+};
 
 export default Formulario;
